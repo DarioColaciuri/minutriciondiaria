15ad84da9abd8636086a055b399253f654957e9f
Dario Colaciuri <darioguitarras@gmail.com>
2025-03-05 16:01:03 -0300

    a

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPAtFwspVleCkprA7_8Ie2FLZpTuep-Lc",
-  authDomain: "minutriciondiaria.firebaseapp.com",
-  projectId: "minutriciondiaria",
-  storageBucket: "minutriciondiaria.firebasestorage.app",
-  messagingSenderId: "1071829435897",
-  appId: "1:1071829435897:web:07a903c1b1bcb3351d2928",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
